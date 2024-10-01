7c41722906750a5181f404ab218121d4771b92a5
meriem <elounimeriem@gmail.com>
2024-10-01 10:59:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BarNav from "./Components/BarNav";
+import Affiche from "./Components/Affiche";
+import Feedback from "./Components/Feedback";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BarNav />
+			<Affiche />
+			<Feedback />
+			<h1>Workshop React JS</h1>
+		</div>
+	);
 }
 
 export default App;
